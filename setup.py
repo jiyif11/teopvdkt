beed5ba62a8d835db0b7bfcd2e7fb40cde3d179e
jiyif11 <132906627+jiyif11@users.noreply.github.com>
2025-04-09 00:22:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -325,8 +325,8 @@ class CMakeBuild(BuildExtension):
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
-            "-DCMAKE_C_FLAGS=-march=armv9-a+fp+nosve+sve+sve2+i8mm+f32mm+f64mm+fp16+bf16+fp16fml+crc",
-            "-DCMAKE_CXX_FLAGS=-march=armv9-a+fp+nosve+sve+sve2+i8mm+f32mm+f64mm+fp16+bf16+fp16fml+crc",
+            "-DCMAKE_C_FLAGS=-march=armv9-a+fp+simd+sve+sve2+i8mm+f32mm+f64mm+fp16+bf16+fp16fml+crc",
+            "-DCMAKE_CXX_FLAGS=-march=armv9-a+fp+simd+sve+sve2+i8mm+f32mm+f64mm+fp16+bf16+fp16fml+crc",
         ]
 
         if CUDA_HOME is not None:
