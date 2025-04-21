d2b8e69efd3a47ab500f6b5c11501eb0c7bedb02
jiyif11 466 <jiyif@qq.com>
2025-04-21 11:49:18 +0000

    fsq

--- third_party/llamafile/iqk_mul_mat_arm82.cpp
+++ third_party/llamafile/iqk_mul_mat_arm82.cpp
@@ -4,7 +4,7 @@
 // Copyright(c) 2024 by KVCache.AI, All Rights Reserved.
 
 #ifdef __aarch64__
-#define iqk_mul_mat iqk_mul_mat_arm82
+//#define iqk_mul_mat iqk_mul_mat_arm82
 #define iqk_mul_mat_moe iqk_mul_mat_moe_arm82
 #include "iqk_mul_mat.inc"
 #endif // __aarch64__
